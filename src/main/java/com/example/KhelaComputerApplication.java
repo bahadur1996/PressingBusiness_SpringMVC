af9340455101419a98d0f913405a9418c4225da2
bahadur1996 <bahadur.arshad@gmail.com>
2020-12-19 19:09:00 +0600

    project structure

---
+++ src/main/java/com/example/KhelaComputerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhelaComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhelaComputerApplication.class, args);
+	}
+}
