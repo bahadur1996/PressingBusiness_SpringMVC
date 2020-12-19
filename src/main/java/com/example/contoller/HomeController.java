06a6c59d4baa0175923062fa997e527a455aaffb
bahadur1996 <bahadur.arshad@gmail.com>
2020-12-20 02:14:19 +0600

    Jpa integration completed.

--- src/main/java/com/example/contoller/HomeController.java
+++ src/main/java/com/example/contoller/HomeController.java
@@ -1,18 +1,10 @@
 package com.example.contoller;
 
-import com.example.entity.UserEntity;
-import com.example.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    private final UserRepository userRepository;
-
-    public HomeController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @RequestMapping("/")
     public String home(){
@@ -20,9 +12,6 @@ public class HomeController {
 }
     @RequestMapping("/index")
     public String index(){
-      UserEntity userEntity =  new UserEntity().setLastName("Bahadur").setFirstName("Arshad").setAddress("Duri angul lan").setCountry("Bangladesh")
-              .setEmail("bahadur@gmail.com").setMobile("+01843135687");
-        userRepository.save(userEntity);
 
         return "index";
     }
